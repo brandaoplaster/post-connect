985d6fc26cd2c5c3ddac6622a1490134ddf995ec
brandaoplaster <brandaoplaster@gmail.com>
2024-07-03 22:17:48 -0300

    feat: Adding controller to user

---
+++ api/src/router/controllers/users_controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func Create(write http.ResponseWriter, request *http.Request) {
+
+}
+
+func Index(write http.ResponseWriter, request *http.Request) {
+
+}
+
+func Show(write http.ResponseWriter, request *http.Request) {
+
+}
+
+func Update(write http.ResponseWriter, request *http.Request) {
+
+}
+
+func Delete(write http.ResponseWriter, request *http.Request) {
+
+}
