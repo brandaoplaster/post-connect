c1077fb665296c8ab20b28bdc06131c998d9ab87
brandaoplaster <brandaoplaster@gmail.com>
2024-07-03 22:20:26 -0300

    feat: Configure route user

--- api/src/router/routes/users.go
+++ api/src/router/routes/users.go
@@ -1,46 +1,39 @@
 package routes
 
-import "net/http"
+import (
+	"api/api/src/router/controllers"
+	"net/http"
+)
 
 var userRoutes = []Route{
 	{
-		URI:    "/users",
-		Method: http.MethodPost,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:           "/users",
+		Method:        http.MethodPost,
+		Function:      controllers.Create,
 		Authenticated: false,
 	},
 	{
-		URI:    "/users",
-		Method: http.MethodGet,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:           "/users",
+		Method:        http.MethodGet,
+		Function:      controllers.Index,
 		Authenticated: false,
 	},
 	{
-		URI:    "/users/{userId}",
-		Method: http.MethodGet,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:           "/users/{userId}",
+		Method:        http.MethodGet,
+		Function:      controllers.Show,
 		Authenticated: false,
 	},
 	{
-		URI:    "/users/{userId}",
-		Method: http.MethodPut,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:           "/users/{userId}",
+		Method:        http.MethodPut,
+		Function:      controllers.Update,
 		Authenticated: false,
 	},
 	{
-		URI:    "/users/{userId}",
-		Method: http.MethodDelete,
-		Function: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:           "/users/{userId}",
+		Method:        http.MethodDelete,
+		Function:      controllers.Delete,
 		Authenticated: false,
 	},
 }
