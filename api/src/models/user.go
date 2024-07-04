0c5fe2e0dcbcec7695b25914891d921faab6421f
brandaoplaster <brandaoplaster@gmail.com>
2024-07-04 19:39:55 -0300

    feat: Adding model to user

---
+++ api/src/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint64    `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	NickName  string    `json:"nickname,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	CreatedAt time.Time `json:"created_at" valid:"-"`
+	UpdatedAt time.Time `json:"updated_at" valid:"-"`
+}
