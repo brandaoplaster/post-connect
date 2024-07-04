d83a8dbecc07088c223abcd322eb577b78dea121
brandaoplaster <brandaoplaster@gmail.com>
2024-07-03 22:31:37 -0300

    feat: Adding router and routes configuration

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,8 +1,12 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/api/src/router/routes"
 
+	"github.com/gorilla/mux"
+)
 
-func Generate() *mux.Router  {
-	return mux.NewRouter()
+func Generate() *mux.Router {
+	router := mux.NewRouter()
+	return routes.ConfigureRoutes(router)
 }
