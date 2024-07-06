6eadd7fabd168ebf93596321947880ffb01d5cd4
brandaoplaster <brandaoplaster@gmail.com>
2024-07-06 20:36:22 -0300

    feat: Adding create function in user repository

--- api/src/repositories/user_repository.go
+++ api/src/repositories/user_repository.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "database/sql"
+import (
+	"api/api/src/models"
+	"database/sql"
+)
 
 type Users struct {
 	database *sql.DB
@@ -8,4 +11,27 @@ type Users struct {
 
 func NewUserRepository(database *sql.DB) *Users {
 	return &Users{database}
+}
+
+func (repositiry Users) Create(user models.User) (uint64, error) {
+	statement, erro := repositiry.database.Prepare(
+		"insert into users (name, nickname, email, password) values(?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	result, erro := statement.Exec(user.Name, user.NickName, user.Email, user.Password)
+	if erro != nil {
+		return 0, erro
+	}
+
+	lastInsert, erro := result.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(lastInsert), nil
+
 }
