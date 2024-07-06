b7789f97752cf786b542956b946bba2b4e39775b
brandaoplaster <brandaoplaster@gmail.com>
2024-07-05 22:47:01 -0300

    feat: Adding config

---
+++ api/src/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	APIURL  = ""
+	PORT    = 0
+	HashKey []byte
+	BlokKey []byte
+)
+
+func Load() {
+	var erro error
+
+	if erro = godotenv.Load; erro != nil {
+		log.Fatal(erro)
+	}
+
+	PORT, erro = strconv.Atoi("APP_PORT")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	APIURL = os.Getenv("API_URL")
+	HashKey = []byte(os.Getenv("HASH_KEY"))
+	BlokKey = []byte(os.Getenv("BLOCK_KEY"))
+}
