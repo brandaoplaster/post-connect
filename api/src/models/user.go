ae4ca9678ce5704d9ad92a83ac7d837c4ea0c00c
brandaoplaster <brandaoplaster@gmail.com>
2024-07-06 20:35:29 -0300

    feat: Adding password to model user

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -7,6 +7,7 @@ type User struct {
 	Name      string    `json:"name,omitempty"`
 	NickName  string    `json:"nickname,omitempty"`
 	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at" valid:"-"`
 	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
