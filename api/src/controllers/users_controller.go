24fbe43d9866372e3c89c6b5244d71cb59237d82
brandaoplaster <brandaoplaster@gmail.com>
2024-07-07 19:52:38 -0300

    feat: Adding create function in user controller

--- api/src/controllers/users_controller.go
+++ api/src/controllers/users_controller.go
@@ -1,9 +1,48 @@
 package controllers
 
-import "net/http"
+import (
+	"api/api/src/database"
+	"api/api/src/models"
+	"api/api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func Create(write http.ResponseWriter, request *http.Request) {
+	response, erro := io.ReadAll(request.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
 
+	var user models.User
+	if erro = json.Unmarshal(response, &user); erro != nil {
+		log.Fatal(erro)
+		return
+	}
+
+	if erro = user.Prepare("Create"); erro != nil {
+		log.Fatal(erro)
+		return
+	}
+
+	db, erro := database.Connect()
+	if erro != nil {
+		log.Fatal(erro)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.NewUserRepository(db)
+	user.ID, erro = repository.Create(user)
+	if erro != nil {
+		log.Fatal(erro)
+		return
+	}
+
+	write.Write([]byte(fmt.Sprintf("id insert %d", user.ID)))
 }
 
 func Index(write http.ResponseWriter, request *http.Request) {
