947b152fc31768642005c37ef4d11a598be90d93
brandaoplaster <brandaoplaster@gmail.com>
2026-06-11 21:27:12 -0300

    refactor: Adjust user routes

--- api/src/router/routes/users.go
+++ api/src/router/routes/users.go
@@ -1,39 +1,40 @@
 package routes
 
 import (
-	"api/api/src/controllers"
 	"net/http"
 )
 
-var userRoutes = []Route{
-	{
-		URI:           "/users",
-		Method:        http.MethodPost,
-		Function:      controllers.Create,
-		Authenticated: false,
-	},
-	{
-		URI:           "/users",
-		Method:        http.MethodGet,
-		Function:      controllers.Index,
-		Authenticated: false,
-	},
-	{
-		URI:           "/users/{userId}",
-		Method:        http.MethodGet,
-		Function:      controllers.Show,
-		Authenticated: false,
-	},
-	{
-		URI:           "/users/{userId}",
-		Method:        http.MethodPut,
-		Function:      controllers.Update,
-		Authenticated: false,
-	},
-	{
-		URI:           "/users/{userId}",
-		Method:        http.MethodDelete,
-		Function:      controllers.Delete,
-		Authenticated: false,
-	},
+func userRoutes(handler *Handlers) []Route{
+	return []Route{
+		{
+			URI:           "/users",
+			Method:        http.MethodPost,
+			Function:      handler.Users.Create,
+			Authenticated: false,
+		},
+		{
+			URI:           "/users",
+			Method:        http.MethodGet,
+			Function:      handler.Users.Index,
+			Authenticated: false,
+		},
+		{
+			URI:           "/users/{userId}",
+			Method:        http.MethodGet,
+			Function:      handler.Users.Show,
+			Authenticated: false,
+		},
+		{
+			URI:           "/users/{userId}",
+			Method:        http.MethodPut,
+			Function:      handler.Users.Update,
+			Authenticated: false,
+		},
+		{
+			URI:           "/users/{userId}",
+			Method:        http.MethodDelete,
+			Function:      handler.Users.Delete,
+			Authenticated: false,
+		},
+	}
 }
