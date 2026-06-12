f6ee543a6d7545d8c5107dcd95bf70eef1bc9edc
brandaoplaster <brandaoplaster@gmail.com>
2026-06-11 21:28:07 -0300

    refactor: Restructure routing system

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,12 +1,11 @@
 package router
 
 import (
-	"api/api/src/router/routes"
-
-	"github.com/gorilla/mux"
+    "api/api/src/router/routes"
+    "github.com/gorilla/mux"
 )
 
-func Generate() *mux.Router {
-	router := mux.NewRouter()
-	return routes.ConfigureRoutes(router)
+func Generate(h *routes.Handlers) *mux.Router {
+    router := mux.NewRouter()
+    return routes.ConfigureRoutes(router, h)
 }
