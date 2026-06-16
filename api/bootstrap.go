05a08d26d977d1229ac1a02e99852907e7e947aa
brandaoplaster <brandaoplaster@gmail.com>
2026-06-16 17:33:01 -0300

    refactor: Adjust bootstrap in NewHandlers function

--- api/bootstrap.go
+++ api/bootstrap.go
@@ -4,13 +4,15 @@ import (
     "api/api/src/controllers"
     "api/api/src/repositories"
     "api/api/src/router/routes"
+    "api/api/src/services"
     "database/sql"
 )
 
 func NewHandlers(db *sql.DB) *routes.Handlers {
     userRepo := repositories.NewUserRepository(db)
+    userService := services.NewUserService(userRepo)
 
     return &routes.Handlers{
-        Users: controllers.NewUsersController(userRepo),
+        Users: controllers.NewUsersController(userService),
     }
 }
